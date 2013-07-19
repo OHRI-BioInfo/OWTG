945720f81e7e2e2ab4e50cda1fa58465b3f8ee84
Frogging101 <Brooks572@gmail.com>
2013-07-19 11:38:45 -0400

    save_alias now copies internal alias file to external file

--- save_alias.php
+++ save_alias.php
@@ -1,5 +1,4 @@
 <?php
-
 include "settings.php";
 include "/opt/owfs/share/php/OWNet/ownet.php";
 
@@ -8,7 +7,11 @@ $address = $_GET["address"];
 
 $ow = new OWNet($adapter);
 
-$ow->set("/".$address."/alias",$alias);
+#Set the alias
+$ow->set("/".$address."/alias",$alias); 
+#Copy the internal alias file to the external one, to preserve aliases
+$fileData = $ow->read($aliasFile);
+file_put_contents($externalAliasFile,$fileData);
 
 unset($ow);
 
--- settings.php
+++ settings.php
@@ -1,4 +1,5 @@
 <?php
 $aliasFile = "/settings/alias/list";
+$externalAliasFile = "/etc/owfs-aliases";
 $adapter = "localhost:4304";
 ?>

7f409dac284f0a263fc8d3ad6dde9f3f9d38c409
Frogging101 <Brooks572@gmail.com>
2013-08-06 14:14:16 -0400

    [python] Move some reusable code into owtg module

--- scripts/owtg.py
+++ scripts/owtg.py
@@ -1,2 +1,23 @@
 sFilename = '/etc/owtg/sensors'
 dbFilename = '/etc/owtg/database.rrd'
+
+def getSensors(graphOnly=False):
+    dAddresses = [] #already discovered addresses
+    gAddresses = [] #Addresses with "graph" turned on
+    
+    sFile = open(sFilename,'r') #discovered file, open for reading
+    lineList = sFile.readlines()
+    sFile.close()
+
+    for line in lineList:
+        #ignore comments (lines beginning with #)
+        if line.startswith('#'):
+            continue
+        if line:
+            line = line.rstrip('\n')
+            dAddresses.append(line.split(':')[1])
+            if line.split(':')[3] == 'y':
+                gAddresses.append(line.split(':')[1])
+    if graphOnly:
+        return gAddresses
+    return dAddresses
